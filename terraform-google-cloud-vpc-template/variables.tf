7588582cd725e87c4988293919835936531ea87e
Niketa184 <59533323+Niketa184@users.noreply.github.com>
2024-11-07 04:42:44 +0530

    Update variables.tf

--- terraform-google-cloud-vpc-template/variables.tf
+++ terraform-google-cloud-vpc-template/variables.tf
@@ -44,7 +44,7 @@ variable "firewall" {
 }
 
 #............................................ VPC Peering ..............................................#
-/*
+
 variable "vpc_peering" {
     sensitive   = false
 }
@@ -53,4 +53,6 @@ variable "vpc_peering" {
 
 variable "route" {
     sensitive   = false
-}*/
+}
+
+*/
