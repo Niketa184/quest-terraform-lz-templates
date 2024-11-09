25ff783d2a1801684ca8e79ae2cd6373f0d65d09
Niketa184 <59533323+Niketa184@users.noreply.github.com>
2024-11-10 03:18:12 +0530

    Update subnetwork.tf

--- terraform-google-cloud-vpc-template/subnetwork.tf
+++ terraform-google-cloud-vpc-template/subnetwork.tf
@@ -4,7 +4,7 @@ module "vpc_subnets" {
   for_each     = {
     for k, v in try(var.subnets,{}): k => v if v.delete != true
   }
-  source                                = "git::https://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@github.com/Niketa184/quest-terraform-lz-modules.git//terraform-google-cloud-subnet"
+  source                                = "git::https://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@github.com/Niketa184/quest-terraform-lz-modules.git//terraform-google-cloud-subnet?ref=v1.0.0"
   project_id                            = each.value.project_id
   network_name                          = each.key
   subnets                               = each.value.subnets
