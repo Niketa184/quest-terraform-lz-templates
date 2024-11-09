f99e2e3c3c156ad8224390c769b5cf9e2e51073b
Niketa184 <59533323+Niketa184@users.noreply.github.com>
2024-11-10 03:14:02 +0530

    Update firewall.tf

--- terraform-google-cloud-vpc-template/firewall.tf
+++ terraform-google-cloud-vpc-template/firewall.tf
@@ -4,7 +4,7 @@ module "vpc_firewall_rule" {
   for_each     = {
     for k, v in try(var.firewall,{}): k => v if v.delete != true
   }
-  source                                = "git::https://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@github.com/Niketa184/quest-terraform-lz-modules.git//terraform-google-cloud-firewall?ref=v1.0.0.0"
+  source                                = "git::https://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@github.com/Niketa184/quest-terraform-lz-modules.git//terraform-google-cloud-firewall?ref=v1.0.0"
   project_id                            = each.value.project_id 
   network_name                          = each.key
   rules                                 = each.value.rules
